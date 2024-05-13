813f67352080a7e0566e4ac02e424822e4907fee
Denise Rodrigues <deniseroav7@gmail.com>
2024-05-13 17:06:08 -0300

    index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,19 +7,19 @@ listaSelecaoPokemons.forEach(pokemon => {
    
     pokemon.addEventListener('click', () => {
         
-        const cartaoPokemonAberto = document.querySelector('.aberto')
-        cartaoPokemonAberto.classList.remove('aberto')
+const cartaoPokemonAberto = document.querySelector('.aberto')
+    cartaoPokemonAberto.classList.remove('aberto')
 
-        const idPokemonSelecionado = pokemon.attributes.id.value
+const idPokemonSelecionado = pokemon.attributes.id.value
 
-        const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
-        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
-        cartaoPokemonParaAbrir.classList.add('aberto')
+const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+    cartaoPokemonParaAbrir.classList.add('aberto')
 
-        const pokemonAtivoNaListagem = document.querySelector('.ativo')
-        pokemonAtivoNaListagem.classList.remove('ativo')
+const pokemonAtivoNaListagem = document.querySelector('.ativo')
+    pokemonAtivoNaListagem.classList.remove('ativo')
 
-        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
         pokemonSelecionadoNaListagem.classList.add('ativo')
     })
 })
